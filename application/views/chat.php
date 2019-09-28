f9f715a5281bce8980d8aeaebd64e87686a4a3e2
RoV <keranair98@yahoo.com>
2019-09-28 10:35:56 +0700

    update
    mengganti isi chat.php dan menambahkan fungsi chat

--- application/views/chat.php
+++ application/views/chat.php
@@ -1,55 +1,151 @@
-<section id="inner-headline">
-      <div class="container">
-        <div class="row">
-          <div class="span4">
-            <div class="inner-heading">
-              <h2>Get in touch</h2>
-            </div>
-          </div>
-          <div class="span8">
-            <ul class="breadcrumb">
-              <li><a href="#"><i class="icon-home"></i></a><i class="icon-angle-right"></i></li>
-              <li class="active">Contact</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section id="content">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+<?php 
+$waktu = time();
+?>
+<style type="text/css">
+	*{
+		margin: 0px;
+		padding: 0px;
+		font-family: sans-serif;
+		size: 15px;
+	}
 
-      <div class="container">
-        <div class="row">
-          <div class="span12">
-            <h4>Get in touch with us by filling <strong>contact form below</strong></h4>
+	#isi{
+		margin: auto;
+		width: 50%;
+		box-sizing: border-box;
+		border: solid 1px;
+		border-radius: 5px;
+	}
 
-            <form action="" method="post" role="form" class="contactForm">
-              <div id="sendmessage">Your message has been sent. Thank you!</div>
-              <div id="errormessage"></div>
+	.card-header{
+		border: solid 1px;
+		background-color: #666666;
+		color: white;
+		text-align: center;
+	}
 
-              <div class="row">
-                <div class="span4 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validation"></div>
-                </div>
-                <div class="span4 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validation"></div>
-                </div>
-                <div class="span4 form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validation"></div>
-                </div>
-                <div class="span12 margintop10 form-group">
-                  <textarea class="form-control" name="message" rows="12" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validation"></div>
-                  <p class="text-center">
-                    <button class="btn btn-large btn-theme margintop10" type="submit">Submit message</button>
-                  </p>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
+	.card-footer{
+		text-align: center;
+	}
+
+	.card-footer > textarea{
+		width: inherit;
+	}
+
+	.card-body{
+		overflow: auto;
+		height: 20rem;
+	}
+
+	textarea{
+		resize: none;
+		overflow: auto;
+	}
+
+	.col-md-3 > img{
+		width: 10%;
+		border-radius: 80%;
+		border: solid 1px;
+	}
+
+	.row > .col-md-auto{
+		font-size: 10px;
+		margin-left: 100px;
+		margin-top: -5px;
+	}
+
+	#hasil > .row, .card-body > .row{
+		text-align: left;
+		border-bottom: solid 2px;
+		margin: 10px;
+		text-align: initial;
+	}
+
+	#pesan{
+		width: 40rem;
+		height: 5rem;
+		margin: 10px;
+	}
+</style>
+
+<section>
+	<div class="card" id="isi" style="width: 50rem;">
+		<div class="card-header">
+			Customer Services
+		</div>
+
+		<div class="card-body">
+
+			<!-- Guna Penjawab -->
+			<div class="row">
+				<div class="col-md-3">
+					<img src="https://myrealdomain.com/images/customer-service-clip-art-5.png">
+					Customer Services Tersedia, Silahkan Kirimkan Pesan....
+				</div>
+				<div class="col-md-auto">
+					<?php 
+					echo date("h:i",$waktu);
+					?>
+				</div>
+			</div>
+			<!-- Guna Penjawab -->
+
+
+
+
+
+			<!-- Guna Penanya -->
+
+			<!-- <?php foreach ($ngechat as $key => $value): ?>
+				<div id="tanya" class="row" style="text-align: right;">
+					<div class="col-md-3">
+						<?php echo $value->pesan; ?>
+						<img src="http://img.clipartlook.com/customer-clipart-cartoon-female-customer-service-customer-service-service-communication-png-image-and-clipart-551.jpg">
+					</div>
+					<div class="col-md-auto" style="margin-right: 100px;">
+						<?php
+						echo $value->waktu;
+						?>
+					</div>
+				</div>
+				<?php endforeach ?> -->
+
+				<!-- Guna Penanya -->
+
+
+
+
+				<!-- Spawn Q -->
+				<div id="hasil">
+
+				</div>
+				<!-- Spawn Q -->
+
+			</div>
+			<div class="card-footer">
+				<input type="text" name="pesan" id="pesan">
+				<button id="kirim" class="btn btn-md btn-success">
+					KIRIM
+				</button>
+			</div>
+		</div>
+	</section>
+	<script type="text/javascript">
+
+		// var html_awal = '<div id="tanya" class="row" style="text-align: right;"><div class="col-md-3">';
+		// var html_akhir = '<img src="http://img.clipartlook.com/customer-clipart-cartoon-female-customer-service-customer-service-service-communication-png-image-and-clipart-551.jpg"></div><div class="col-md-auto" style="margin-right: 100px;">'+<?php echo date("h:i",$waktu); ?>+'</div></div>';
+
+		$(document).ready(function() {
+
+			var kirim = $('#kirim');
+			var hasil = $('#hasil');
+			var pesan = $('#pesan');
+
+			var dt = new Date();
+			var waktu = dt.getHours()+":"+dt.getMinutes();
+
+			kirim.click(function(){
+				hasil.append('<div id="tanya" class="row" style="text-align: right;"><div class="col-md-3">'+pesan.val()+'<img src="http://img.clipartlook.com/customer-clipart-cartoon-female-customer-service-customer-service-service-communication-png-image-and-clipart-551.jpg"></div><div class="col-md-auto" style="margin-right: 100px;">'+waktu+'</div></div>');
+			})
+		})
+	</script>
